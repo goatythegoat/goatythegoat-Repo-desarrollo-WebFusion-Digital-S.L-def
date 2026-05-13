3d4e0c0a7a93ce33b5e7c66cc7ec7499c0ed7f32
goatythegoat <108833140+goatythegoat@users.noreply.github.com>
2026-05-13 19:15:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 echo "<div style='background: #0073aa; color: white; padding: 20px; text-align: center; font-family: sans-serif;'>";
 echo "<h1>Holacomostan</h1>";
 echo "<p>probando probando.</p>";
+echo "<p>hola</p>";
 echo "</div>";
 
 define( 'WP_USE_THEMES', true );
