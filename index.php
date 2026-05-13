37f95ffa40d0bc1a201a0fcf38671ccafde38de0
goatythegoat <108833140+goatythegoat@users.noreply.github.com>
2026-05-13 18:32:23 +0200

    version 0.1

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+echo "<div style='background: #0073aa; color: white; padding: 20px; text-align: center; font-family: sans-serif;'>";
+echo "<h1>Holacomostan</h1>";
+echo "<p>probando probando.</p>";
+echo "</div>";
+
+define( 'WP_USE_THEMES', true );
+require __DIR__ . '/wp-blog-header.php';
